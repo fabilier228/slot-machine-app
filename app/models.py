079c0c4170dbef816e99a99600f4896365333731
fabilier228 <kunikowskikuba@gmail.com>
2025-01-09 14:55:53 +0100

    Feature - models for database

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from . import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    saldo = db.Column(db.Float(), nullable=False)
+
+class GameLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    result = db.Column(db.String(50), nullable=False)
+    timestamp = db.Column(db.DateTime, default=db.func.now())
