844523f18db75f46c0ba259a341d87fb95ac78ec
fabilier228 <kunikowskikuba@gmail.com>
2025-01-10 15:09:57 +0100

    Feature - route for home endpoint

--- app/views.py
+++ app/views.py
@@ -1,27 +1,7 @@
-from flask import Blueprint, request, jsonify, render_template
-from .models import User, GameLog
-from . import db
+from flask import Blueprint, render_template
 
-main = Blueprint('main', __name__)
+views = Blueprint('views', __name__)
 
-@main.route('/users', methods=['POST'])
-def create_user():
-    data = request.json
-    new_user = User(username=data['username'], password=data['password'])
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify({'message': 'User created'}), 201
-
-@main.route('/users/<int:id>', methods=['GET'])
-def get_user(id):
-    user = User.query.get_or_404(id)
-    return jsonify({'id': user.id, 'username': user.username})
-
-@main.route('/games', methods=['POST'])
-def log_game():
-    data = request.json
-    new_log = GameLog(user_id=data['user_id'], result=data['result'])
-    db.session.add(new_log)
-    db.session.commit()
-    return jsonify({'message': 'Game log created'}), 201
-
+@views.route('/')
+def home():
+    return render_template("home.html")
