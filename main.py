213080271012081a7102e22bf4202f6340b2b8c7
fabilier228 <kunikowskikuba@gmail.com>
2025-01-10 15:10:32 +0100

    Refactor - main.py refactored

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-from app import create_app, db, socketio
-from flask_socketio import emit
-from random import choice
+from app import create_app
 
 app = create_app()
 
-@socketio.on('play_slot')
-def handle_play_slot(data):
-    result = choice(['Win', 'Lose', 'Draw'])
-    emit('slot_result', {'result': result}, broadcast=True)
-
-if __name__ == '__main__':
-    db.create_all(app=create_app())
-    socketio.run(app, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
