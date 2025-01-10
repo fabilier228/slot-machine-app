464bd222ff158cf93322a66f2f0920b79b07bc92
fabilier228 <kunikowskikuba@gmail.com>
2025-01-10 17:52:59 +0100

    Refactor - database tabels adjusted

--- app/models.py
+++ app/models.py
@@ -1,14 +1,18 @@
 from . import db
+from flask_login import UserMixin
 
-class User(db.Model):
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
-    saldo = db.Column(db.Float(), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    saldo = db.Column(db.Float(), nullable=False, default=0.0)
+    games = db.relationship("GameLog")
+
 
 class GameLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    result = db.Column(db.String(50), nullable=False)
-    timestamp = db.Column(db.DateTime, default=db.func.now())
+    result = db.Column(db.Float(), nullable=False)
+    timestamp = db.Column(db.DateTime(timezone=True), default=db.func.now())
