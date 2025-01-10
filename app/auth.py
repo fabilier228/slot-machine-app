a46110b04e4c9f8a15c6ba6e7e6b60cccba10f86
fabilier228 <kunikowskikuba@gmail.com>
2025-01-10 15:10:57 +0100

    Feature - file for authentication routes

---
+++ app/auth.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, render_template, request
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+@auth.route('/sign-up', methods=["GET", "POST"])
+def sign_up():
+    if request.method == "POST":
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+    return render_template("signup.html")
+
+@auth.route('/logout')
+def logout():
+    return "<p>logout</p>"
