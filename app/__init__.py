46e7d5b6ce2d53ba2410dd0609809830c7cd5452
fabilier228 <kunikowskikuba@gmail.com>
2025-01-12 19:52:27 +0100

    Update - migrations package added to the project

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 bcrypt = Bcrypt()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -14,6 +16,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
     db.init_app(app)
+    migrate = Migrate(app, db)
 
 
     from .views import views
