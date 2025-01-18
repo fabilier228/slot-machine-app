2eee572e655323c119425f3def28f8dea1b95d1e
fabilier228 <kunikowskikuba@gmail.com>
2025-01-18 13:12:49 +0100

    Feature/Refactor - placeholder for routes made for sending messages

---
+++ app/sockets.py
@@ -0,0 +1,50 @@
+from flask import Blueprint, jsonify, Response
+from .models import User
+import json
+import random
+import time
+from . import socketio
+from flask_socketio import emit, send
+from threading import Thread
+
+def get_advertisement_from_json():
+    with open('adverts.json', 'r', encoding='utf-8') as f:
+        ads = json.load(f)
+        ad = random.choice(ads)
+        return ad['description']
+
+def generate_advertisements():
+    while True:
+        ad = get_advertisement_from_json()
+        yield f"data: {ad}\n\n"
+        time.sleep(5)
+
+@socketio.on('connect')
+def connect_user():
+    print("connected")
+    # users = User.query.all()
+    #
+    # while True:
+    #     winner = random.choice(users)
+    #     amount_won = random.randint(1, 50000)
+    #
+    #     message = f"🎉 {winner.username} won {amount_won} coins! 🎉"
+    #
+    #     socketio.emit('new_winner', {'message': message})
+    #     time.sleep(5)
+    #
+
+@socketio.on('event')
+def send_random_user_win():
+    users = User.query.all()
+    winner = random.choice(users)
+    amount_win = random.randint(1,5000)
+    message = f"🎉 {winner.username} just won {amount_win}$ ! 🎉"
+    emit('new_winner', {'message': message})
+
+sockets = Blueprint('sockets', __name__)
+
+
+@sockets.route('/events/datetime')
+def stream():
+    return Response(generate_advertisements(), content_type='text/event-stream')
--- app/static/js/socket.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    var es = new EventSource("/events/datetime");
-            es.onmessage = function(event) {
-                const eventDiv = document.getElementById("ad-text");
-                eventDiv.innerHTML = '<div>' + event.data + '</div>';
-            }
-});
